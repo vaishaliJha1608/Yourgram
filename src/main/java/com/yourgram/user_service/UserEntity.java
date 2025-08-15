092363cabd784038f0dd95777eebe0a971e36f61
Vaishali Jha <jhavaishali1606523@gmail.com>
2025-08-15 17:23:34 +0530

    Dependencies Push

---
+++ src/main/java/com/yourgram/user_service/UserEntity.java
@@ -0,0 +1,4 @@
+package com.yourgram.user_service;
+
+public class UserEntity {
+}
---
+++ src/test/java/com/yourgram/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourgram.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

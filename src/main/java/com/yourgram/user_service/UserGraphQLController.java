c646b8f0ae53ac66b7f90cdc3df26f1bde17c16e
Vaishali Jha <jhavaishali1606523@gmail.com>
2025-09-11 07:40:08 +0530

    Get User By Id Changes

--- src/main/java/com/yourgram/user_service/UserGraphQLController.java
+++ src/main/java/com/yourgram/user_service/UserGraphQLController.java
@@ -1,12 +1,11 @@
 package com.yourgram.user_service;
 
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.Arguments;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserGraphQLController {
@@ -20,6 +19,11 @@ public class UserGraphQLController {
     @MutationMapping
     public User registerUser(@Argument RegisterRequest registerRequest) {
         return userService.registerUser(registerRequest);
+    }
+
+    @QueryMapping
+    public Optional<User> userById(@Argument Long id) {
+        return userService.userById(id);
     }
 }
 
--- src/main/java/com/yourgram/user_service/UserService.java
+++ src/main/java/com/yourgram/user_service/UserService.java
@@ -2,7 +2,7 @@ package com.yourgram.user_service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,9 +19,8 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public Optional<User> userById(Long id) {
+        return  userRepository.findById(id);
     }
-
 }
 
